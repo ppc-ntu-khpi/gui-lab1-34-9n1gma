80a0f58d94dba50fbcaa895d4d97189e4566207a
9n1gma <63008089+9n1gma@users.noreply.github.com>
2020-06-16 13:13:54 +0300

    gui-lab1-34-9n1gma

--- Banking/src/com/mybank/gui/SWINGDemo.java
+++ Banking/src/com/mybank/gui/SWINGDemo.java
@@ -1,14 +1,18 @@
 package com.mybank.gui;
 
+import com.mybank.data.DataSource;
+import com.mybank.domain.Account;
 import com.mybank.domain.Bank;
 import com.mybank.domain.CheckingAccount;
 import com.mybank.domain.Customer;
 import com.mybank.domain.SavingsAccount;
+import com.mybank.reporting.CustomerReport;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JEditorPane;
@@ -27,7 +31,7 @@ public class SWINGDemo {
     
     public SWINGDemo() {
         log = new JEditorPane("text/html", "");
-        log.setPreferredSize(new Dimension(250, 150));
+        log.setPreferredSize(new Dimension(250, 350));
         show = new JButton("Show");
         clients = new JComboBox();
         for (int i=0; i<Bank.getNumberOfCustomers();i++)
@@ -51,15 +55,37 @@ public class SWINGDemo {
         show.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Customer current = Bank.getCustomer(clients.getSelectedIndex());
-                String accType = current.getAccount(0)instanceof CheckingAccount?"Checking":"Savings";                
-                String custInfo="<br>&nbsp;<b><span style=\"font-size:2em;\">"+current.getLastName()+", "+
-                        current.getFirstName()+"</span><br><hr>"+
-                        "&nbsp;<b>Acc Type: </b>"+accType+
-                        "<br>&nbsp;<b>Balance: <span style=\"color:red;\">$"+current.getAccount(0).getBalance()+"</span></b>";
-                log.setText(custInfo);                
+                StringBuilder custInfo = new StringBuilder();
+                
+                    Customer current = Bank.getCustomer(clients.getSelectedIndex());            
+                    custInfo.append("<br>&nbsp;<b><span style=\"font-size:2em;\">")
+                            .append(current.getLastName())
+                            .append(", ")
+                            .append(current.getFirstName())
+                            .append("</span><br><hr>");                                                  
+                          
+                    for (int j = 0; j < current.getNumberOfAccounts(); j++) {
+                        custInfo.append("&nbsp;<b>Account Type:  </b>");
+                        Account account = current.getAccount(j);
+                        
+                        if (account instanceof SavingsAccount){ 
+                            custInfo.append("'Savings'");
+                        }
+                        else if (account instanceof CheckingAccount){
+                            custInfo.append("'Checking'");
+                        }
+                        custInfo.append("<br>&nbsp;<b>Balance: <span style=\"color:red;\">$")
+                                .append(account.getBalance())
+                                .append("</span></b></br>").append("<br></br>");
+                    }
+                           
+                   
+                
+                log.setText(custInfo.toString());              
             }
         });
+        
+        
         
         frame.pack();
         frame.setLocationRelativeTo(null);
@@ -68,14 +94,11 @@ public class SWINGDemo {
         frame.setVisible(true);        
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Bank.addCustomer("John", "Doe");
-        Bank.addCustomer("Fox", "Mulder");
-        Bank.addCustomer("Dana", "Scully");
-        Bank.getCustomer(0).addAccount(new CheckingAccount(2000));
-        Bank.getCustomer(1).addAccount(new SavingsAccount(1000, 3));
-        Bank.getCustomer(2).addAccount(new CheckingAccount(1000, 500));
+        DataSource dataSource = new DataSource("\\data\\test.dat");
+        dataSource.loadData();
+       
         
         SWINGDemo demo = new SWINGDemo();        
         demo.launchFrame();
